6e580739ccd321793c10b7d84f2a63a7092998fd
Kazumi Karbowski <kazkazinus@gmail.com>
2018-06-12 03:31:28 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,12 @@
+require 'pry'
+
 html = File.read('fixtures/kickstarter.html')
- 
+
 kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
   # write your code here
 end
+binding.pry
+
+create_project_hash
